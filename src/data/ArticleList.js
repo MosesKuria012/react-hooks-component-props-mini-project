c5f2f7f19650099bc0d227b72b91148cdd49c96e
Moses <t.content012@gmail.com>
2023-04-21 00:02:05 +0300

    inital commit

--- src/data/ArticleList.js
+++ src/data/ArticleList.js
@@ -1,27 +1,7 @@
 import React from 'react'
+import blogData from '../data/blog'
 
-const blogData = [
-  {
-    id: 1,
-    title: 'Components 101',
-    date: 'December 15, 2020',
-    preview: 'Setting up the building blocks of your site',
-    minutes: 5,
-  },
-  {
-    id: 2,
-    title: 'React Data Flow',
-    date: 'December 11, 2020',
-    preview: 'Passing props is never passé',
-    minutes: 15,
-  },
-  {
-    id: 3,
-    title: 'Function vs Class Components',
-    preview: 'React, meet OOJS.',
-    minutes: 47,
-  },
-]
+console.log(blogData)
 
 function App() {
   const blogName = 'Overreacted'
@@ -30,16 +10,15 @@ function App() {
 
   return (
     <div className="App">
-      <Header name={blogName} imageURL={imageURL} titleAtr={titleAtr} />
+      <Header name={blogName} />
 
       <aside>
-        <img src={imageURL} alt={titleAtr} />
+        <img imageURL={imageURL} titleAtr={titleAtr} />
         <p>
           Personal Blog by <a href=" https://overreacted.io/"> Dan Abramov</a>
           <br /> <br />I explain with words and code.
         </p>
       </aside>
-      <ArticleList articles={blogData} />
     </div>
   )
 }
@@ -76,7 +55,5 @@ function Article({ article }) {
     </article>
   )
 }
-
-console.log(blogData)
 
 export default App
